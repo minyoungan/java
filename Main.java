0d520f8ea89fe808d11bcd2a933161afef601549
Minyoung An <minyoung.an@gmail.com>
2022-11-06 21:05:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,3 +7,17 @@ class Main {
     System.out.println("I am " + age + " years old.");
   }
 }
+
+// Static Variables Initialized in Java
+
+
+class Main {
+
+  static int age;
+
+  public static void main(String[] args) {
+
+
+    System.out.println("I am " + age + " years old.");
+  }
+}
